8ed0e761c88e67ed6cb57c03340f4fe73071e2aa
MackenzieCameron7 <54957783+MackenzieCameron7@users.noreply.github.com>
2023-09-28 20:54:24 -0400

    updated rating
    rating now turns red when less than 4 and green when 4 or higher

--- lab_3/src/Listing.jsx
+++ lab_3/src/Listing.jsx
@@ -1,14 +1,14 @@
 export default function Listing({pic, country, location, rating, price}){
 
-    //const styles = {color: }
+    const styles = {color: rating < 4 && "red"}
 
     return(
         <div className="Listing">
             <img src={pic} alt="" width="120"/>
            <div className="Listing-Text">
                 <h2>{country}</h2>
-                <p>{location}</p>
-                <p>{rating}&#9733;</p>
+                <div className="location"><p>{location}</p></div>
+                {rating &&<div className="rating" style={styles}><p>{rating}&#9733;</p></div>}
                 <p>${price}/night</p>
             </div>
         </div>
